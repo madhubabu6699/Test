b617425425ff4394af7cc9d5910b6bf28dbf0342
OJAS-INTERVIEW <interview@corp.ojas-it.com>
2019-10-30 22:15:01 +0530

    Test Added

--- AItest/src/app/ai.service.ts
+++ AItest/src/app/ai.service.ts
@@ -10,7 +10,8 @@ export class AIService {
 
   constructor(private http:HttpClient) { }
   getMethod(){
-    return this.http.get("https://hn.algolia.com/api/v1/search_by_data?tags=story")
+    return this.http.get("https://hn.algolia.com/api/v1/search_by_date?tags=story");
   }
 }
+
 
